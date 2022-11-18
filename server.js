1112b4e8b5378616fc14c216100a1512a097a234
Glitch (glitch-hello-node) <none>
2022-11-18 07:57:35 +0000

    📕🏦 Checkpoint ./tambourine.html:48933766/1 ./server.js:48933766/3495

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 //routing our landing page to login.html
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
-
-
 
 /*
 
@@ -19,42 +17,47 @@ there's a lot more network security magic that goes on in the background in thso
 
 */
 
-
-app.get('/piano', (req, res) => {              //user = gura
-    res.sendFile(__dirname + '/piano.html');
+app.get("/piano", (req, res) => {
+  //user = gura
+  res.sendFile(__dirname + "/piano.html");
 });
 
-app.get('/snare', (req, res) => {              //user = snare
-    res.sendFile(__dirname + '/snare.html');
+app.get("/snare", (req, res) => {
+  //user = snare
+  res.sendFile(__dirname + "/snare.html");
 });
 
-app.get('/tambourine', (req, res) => {              //user = tambourine
-    res.sendFile(__dirname + '/tambourine.html');
+app.get("/tambourine", (req, res) => {
+  //user = tambourine
+  res.sendFile(__dirname + "/tambourine.html");
 });
 
 // connect the server to port 3000
 http.listen(3000, () => {
-    console.log('Connected at 3000');
+  console.log("Connected at 3000");
 });
 
 //when a client is connected
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-  
-    //forward data
-    socket.on('key', (msg) => {
-        console.log(msg);
-        socket.broadcast.emit("key", msg);
-    });
-  
-      socket.on('snare', (msg) => {
-        console.log(msg);
-        socket.broadcast.emit("snare", msg);
-    });
-    
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-  
+  //forward data
+  socket.on("key", (msg) => {
+    console.log(msg);
+    socket.broadcast.emit("key", msg);
+  });
+
+  socket.on("snare", (msg) => {
+    console.log(msg);
+    socket.broadcast.emit("snare", msg);
+  });
+
+  socket.on("tambourine", (msg) => {
+    console.log(msg);
+    socket.broadcast.emit("tambourine", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
